b3bb4a6830571b422f4edfeeab27044229fb90a4
ben <rotted_ben@protonmail.com>
2020-11-23 14:27:50 +0100

    checking new list monitor

--- src/libraries/scrap_websites_util.py
+++ src/libraries/scrap_websites_util.py
@@ -80,7 +80,7 @@ def filter_tweets(all_tweets):
 def query_tweets(twitter, token):
     if token[0] == '@':  # actually searching for a user
         print("showing user tweets: " + token)
-        return twitter.get_user_timeline(screen_name=token[1:])
+        return twitter.get_user_timeline(screen_name=token[1:], count=20)
         # return twitter.show_user(screen_name=token[1:])
     else:
         return twitter.search(q='$' + token)
