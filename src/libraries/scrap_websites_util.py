c737295adcc722f41adf1246f624d72dbc842295
ben <rotted_ben@protonmail.com>
2020-11-23 18:39:22 +0100

    working on twitter

--- src/libraries/scrap_websites_util.py
+++ src/libraries/scrap_websites_util.py
@@ -59,7 +59,7 @@ def format_tweet(tweet, is_user_tweet: bool = False):
 
 def get_last_tweets(twitter, ticker):
     if ticker[0] == "@":
-        message = "<b>Last tweets by @ " + ticker[1:] + ":</b>\n"
+        message = '<b>Last tweets by @ <a href="twitter.com/"' + ticker + '">' + ticker[1:] + "</a>:</b>\n"
         try:
             results = query_tweets(twitter, ticker[1:], True)
         except TwythonError:
@@ -68,7 +68,6 @@ def get_last_tweets(twitter, ticker):
         tweets = []
         for tweet in results:
             tweets.append(format_tweet(tweet))
-        pprint(tweets)
         return message + ''.join(tweets)
     else:
         try:
