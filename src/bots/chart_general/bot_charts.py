6e8213f258a7cd4b5f8728346905c94d8b5f46e7
ben <rotted_ben@protonmail.com>
2020-11-23 13:58:08 +0100

    checking new list monitor

--- src/bots/chart_general/bot_charts.py
+++ src/bots/chart_general/bot_charts.py
@@ -606,7 +606,7 @@ def callback_minute(context: CallbackContext):
         new_list = dict()
         for c in channels_to_check:
             if c[1].lower() in new_list:
-                new_list[c[1].lower()] = new_list.get(c[1].lower()).append(c[0])
+                new_list[c[1].lower()] = new_list.get(c[1].lower()) + c[0]
             else:
                 new_list[c[1].lower()] = [c[0]]
         pprint.pprint(new_list)
