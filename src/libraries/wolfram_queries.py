d8d8b922adec51a5cdf3d74f0fe02f7567be5f06
ben <rotted_ben@protonmail.com>
2020-11-24 10:22:03 +0100

    added wolfram support

--- src/libraries/wolfram_queries.py
+++ src/libraries/wolfram_queries.py
@@ -3,9 +3,9 @@ from pprint import pprint
 
 def ask_wolfram_raw(query, client):
     res = client.query(query)
-    pprint(res)
     for pod in res.pods:
 
+        pprint(pod)
         if pod['@id'] == 'Result':
             pprint(pod['subpod'])
             return pod['subpod']['plaintext']
