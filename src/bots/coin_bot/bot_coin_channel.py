9f7f4087607be6fd84aab781dfe3a580d021502c
ben <rotted_ben@protonmail.com>
2020-11-26 20:11:23 +0100

    added list

--- src/bots/coin_bot/bot_coin_channel.py
+++ src/bots/coin_bot/bot_coin_channel.py
@@ -189,7 +189,7 @@ def callback_minute_check_buys(context: CallbackContext):
         last_min = now - 80
 
         options = ["print_complex"]
-        pair = web3_util.does_pair_token_eth_exist(channel.contract, uni_wrapper)
+        pair = channel.pair_contract
         latest_actions_pretty = requests_util.pretty_print_monitor_last_actions(last_min, pair.lower(), graphql_client_uni, options, amount=100)
         pprint.pprint("latest actions for coin " + str(channel.contract))
         pprint.pprint(latest_actions_pretty)
