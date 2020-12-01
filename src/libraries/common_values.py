a62b0b599d7fdbb59dd64d66c8c1a3000b7c96c9
ben <rotted_ben@protonmail.com>
2020-12-01 07:34:05 +0100

    added default binance values

--- src/libraries/common_values.py
+++ src/libraries/common_values.py
@@ -1,3 +1,105 @@
 lambo_price_usd = 220000
 
 test_error_token = "Looks like you need to either: increase slippage and/or remove some decimals from the amount of token you're trying to buy"
+
+# chart values
+
+btc = ['binance', 'BTCUSDT']
+eth = ['binance', 'ETHUSDT']
+ripple = ['binance', 'XRPUSDT']
+litecoin = ['binance', 'LTCUSDT']
+link = ['binance', 'LINKUSDT']
+xlm = ['binance', 'XLMUSDT']
+ada = ['binance', 'ADAUSDT']
+busd = ['binance', 'BUSDUSDT']
+bch = ['binance', 'BCHUSDT']
+bnb = ['binance', 'BNBUSDT']
+eos = ['binance', 'EOSUSDT']
+sxp = ['binance', 'SXPUSDT']
+yfi = ['binance', 'YFIUSDT']
+trx = ['binance', 'TRXUSDT']
+dot = ['binance', 'DOTUSDT']
+etc = ['binance', 'ETCUSDT']
+usdc = ['binance', 'USDCUSDT']
+xtz = ['binance', 'XTZUSDT']
+dash = ['binance', 'DASHUSDT']
+xrpup = ['binance', 'XRPUPUSDT']
+fil = ['binance', 'FILUSDT']
+zec = ['binance', 'ZECUSDT']
+xrpdown = ['binance', 'XRPDOWNUSDT']
+aave = ['binance', 'AAVEUSDT']
+vet = ['binance', 'VETUSDT']
+yfii = ['binance', 'YFIIUSDT']
+band = ['binance', 'BANDUSDT']
+neo = ['binance', 'NEOUSDT']
+xmr = ['binance', 'XMRUSDT']
+comp = ['binance', 'COMPUSDT']
+skl = ['binance', 'SKLUSDT']
+sxpup = ['binance', 'SXPUPUSDT']
+crv = ['binance', 'CRVUSDT']
+omg = ['binance', 'OMGUSDT']
+zen = ['binance', 'ZENUSDT']
+iota = ['binance', 'IOTAUSDT']
+zil = ['binance', 'ZILUSDT']
+atom = ['binance', 'ATOMUSDT']
+btt = ['binance', 'BTTUSDT']
+btcdown = ['binance', 'BTCDOWNUSDT']
+ont = ['binance', 'ONTUSDT']
+pax = ['binance', 'PAXUSDT']
+qtum = ['binance', 'QTUMUSDT']
+jst = ['binance', 'JSTUSDT']
+ethup = ['binance', 'ETHUPUSDT']
+btcup = ['binance', 'BTCUPUSDT']
+waves = ['binance', 'WAVESUSDT']
+chart_dictionary = {"BTC": btc,
+                    "BITCOIN": btc,
+                    "ETH": eth,
+                    "ETHER": eth,
+                    "ETHEREUM": eth,
+                    "XRP": ripple,
+                    "RIPPLE": ripple,
+                    "LITECOIN": litecoin,
+                    "LTC": litecoin,
+                    "LINK": link,
+                    "XLM": xlm,
+                    "ADA": ada,
+                    "BUSD": busd,
+                    "BCH": bch,
+                    "BNB": bnb,
+                    "EOS": eos,
+                    "SXP": sxp,
+                    "YFI": yfi,
+                    "TRX": trx,
+                    "DOT": dot,
+                    "ETC": etc,
+                    "USDC": usdc,
+                    "XTZ": xtz,
+                    "DASH": dash,
+                    "XRPUP": xrpup,
+                    "FIL": fil,
+                    "ZEC": zec,
+                    "XRPDOWN": xrpdown,
+                    "AAVE": aave,
+                    "VET": vet,
+                    "YFII": yfii,
+                    "BAND": band,
+                    "NEO": neo,
+                    "XMR": xmr,
+                    "COMP": comp,
+                    "SKL": skl,
+                    "SXPUP": sxpup,
+                    "CRV": crv,
+                    "OMG": omg,
+                    "ZEN": zen,
+                    "IOTA": iota,
+                    "ZIL": zil,
+                    "ATOM": atom,
+                    "BTT": btt,
+                    "BTCDOWN": btcdown,
+                    "ONT": ont,
+                    "PAX": pax,
+                    "QTUM": qtum,
+                    "JST": jst,
+                    "ETHUP": ethup,
+                    "BTCUP": btcup,
+                    "WAVES": waves}
--- src/libraries/graphs_util.py
+++ src/libraries/graphs_util.py
@@ -11,6 +11,7 @@ sys.path.insert(1, BASE_PATH + '/telegram-bots/src')
 import pandas as pd
 import libraries.requests_util as requests_util
 import libraries.util as util
+from libraries.common_values import chart_dictionary
 import numpy as np
 import plotly.io as pio
 from PIL import Image, ImageDraw, ImageFont, ImageOps
@@ -152,7 +153,6 @@ def __process_and_write_candlelight(dates, openings, closes, highs, lows, volume
     fig['layout']['showlegend'] = False
     fig['layout']['margin'] = dict(t=15, b=15, r=15, l=15)
 
-
     if options is not None:
         if "bband" in options:
             ress = bollinger_bands(highs, lows, closes)
@@ -213,13 +213,9 @@ def __process_and_write_candlelight(dates, openings, closes, highs, lows, volume
     fig['data'].append(dict(x=dates, y=volumes,
                             marker=dict(color=colors_volume),
                             type='bar', yaxis='y', name='Volume'))
-    t0 = time.time()
+
     img = pio.to_image(fig=fig, scale=2)
-    t1 = time.time()
-    t = io.BytesIO(img)
-    pprint.pprint("actual saving")
-    pprint.pprint(t1 - t0)
-    return t
+    return io.BytesIO(img)
 
 
 # t_from and t_to should be numbers, not strings
@@ -350,14 +346,13 @@ def __get_concat_v(im1, im2):
 
 
 def add_border(img, color):
-    return ImageOps.expand(img,border=10,fill=color)
+    return ImageOps.expand(img, border=10, fill=color)
 
 
 # t_from and t_to should be int epoch second
 # return the last price
 # options = TA stuff for example
 def print_candlestick(token, t_from, t_to, file_path, txt: str = None, options=None):
-    t0 = time.time()
     resolution = __calculate_resolution_from_time(t_from, t_to)
     check_others = True
     if options is not None:
@@ -366,46 +361,25 @@ def print_candlestick(token, t_from, t_to, file_path, txt: str = None, options=N
             values = requests_util.get_binance_chart_data(token.upper() + "USDT", t_from, t_to)
             (date_list, opens, closes, highs, lows, volumes) = __preprocess_binance_charts_data(values)
     if check_others:
-        if token.upper() == "BTC":
-            values = requests_util.get_binance_chart_data("BTCUSDT", t_from, t_to)
-            (date_list, opens, closes, highs, lows, volumes) = __preprocess_binance_charts_data(values)
-        elif token.upper() == "ETH" or token.upper() == "WETH" or token.upper() == "ETHEREUM":
-            values = requests_util.get_binance_chart_data("ETHUSDT", t_from, t_to)
-            (date_list, opens, closes, highs, lows, volumes) = __preprocess_binance_charts_data(values)
-        elif token.upper() == "XRP" or token.lower() == "ripple":
-            values = requests_util.get_binance_chart_data("XRPUSDT", t_from, t_to)
-            (date_list, opens, closes, highs, lows, volumes) = __preprocess_binance_charts_data(values)
-        elif token.upper() == "LTC" or token.lower() == "litecoin":
-            values = requests_util.get_binance_chart_data("LTCUSDT", t_from, t_to)
-            (date_list, opens, closes, highs, lows, volumes) = __preprocess_binance_charts_data(values)
+        if token.upper() in chart_dictionary:
+            token_entry = chart_dictionary[token.upper()]
+            if token_entry[0] == 'binance':
+                values = requests_util.get_binance_chart_data(token_entry[1], t_from, t_to)
+                (date_list, opens, closes, highs, lows, volumes) = __preprocess_binance_charts_data(values)
+            else:  # defaulting to chartex
+                values = requests_util.get_graphex_data(token, resolution, t_from, t_to).json()
+                (date_list, opens, closes, highs, lows, volumes) = __preprocess_chartex_data(values, resolution)
         else:
             values = requests_util.get_graphex_data(token, resolution, t_from, t_to).json()
             (date_list, opens, closes, highs, lows, volumes) = __preprocess_chartex_data(values, resolution)
-    t1 = time.time()
     chart_img_raw = __process_and_write_candlelight(date_list, opens, closes, highs, lows, volumes, file_path, token, options)
-    t2 = time.time()
     chart_img = Image.open(chart_img_raw)
-    t3 = time.time()
     if txt is not None:
         img_up = __generate_upper_barrier(txt, options)
         chart_img = __get_concat_v(img_up, chart_img)
     border_color = '#013220' if closes[-1] > closes[0] else '#3f0000'
     img_final = add_border(chart_img, color=border_color)
-    t4 = time.time()
     img_final.save(file_path)
-    t5 = time.time()
-    pprint.pprint("fetch data and preprocess")
-    pprint.pprint(t1 - t0)
-    pprint.pprint("write img")
-    pprint.pprint(t2 - t1)
-    pprint.pprint("open raw img")
-    pprint.pprint(t3 - t2)
-    pprint.pprint("add barrier")
-    pprint.pprint(t4 - t3)
-    pprint.pprint("save image")
-    pprint.pprint(t5 - t4)
-    pprint.pprint("total")
-    pprint.pprint(t5 - t0)
     return closes[-1]
 
 
@@ -429,10 +403,6 @@ def main():
     t_to = int(time.time())
     t_from = int(time.time()) - 3600*24
     # print_candlestick(token, t_from, t_to, "testaaa2.png", "coucou", ["bband"])
-    print_candlestick(token, t_from, t_to, "testaaa2.png", "coucou", ["dark", "m"])
-    pprint.pprint("-------------")
-    print_candlestick(token, t_from, t_to, "testaaa2.png", "coucou", ["dark", "m"])
-    pprint.pprint("-------------")
     print_candlestick(token, t_from, t_to, "testaaa2.png", "coucou", ["dark", "m"])
 
 
