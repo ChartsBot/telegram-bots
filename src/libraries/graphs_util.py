40c9506c1424fcb83ce1640a64afb0731cfc6170
ben <rotted_ben@protonmail.com>
2020-12-07 14:25:22 +0100

    trying to improve charts

--- src/libraries/graphs_util.py
+++ src/libraries/graphs_util.py
@@ -392,7 +392,9 @@ def __preprocess_chartex_data(values, resolution):
                 else:
                     # those 2 lines here to fix strange chartex behaviour
                     opens[index] = closes[index - 1]
-                    lows[index] = opens[index] if opens[index] < lows[index] else lows[index]
+                    pprint("open: " + str(opens[index]) + " - closes before: " + str(closes[index - 1]))
+                    lows[index] = min([opens[index], lows[index], closes[index]])
+                    highs[index] = max([opens[index], highs[index], closes[index]])
                     if highs[index] > highs[index - 1] * 2 and highs[index] > highs[index + 1] * 2:
                         # print("reducing highs")
                         highs[index] = (highs[index - 1] + highs[index + 1])
