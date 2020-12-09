92a87c75506d7a77a5cda3c366b96f640eb93f24
ben <rotted_ben@protonmail.com>
2020-12-09 22:06:33 +0100

    christmas

--- src/bots/chart_general/bot_charts.py
+++ src/bots/chart_general/bot_charts.py
@@ -433,8 +433,8 @@ def get_time_to(update: Update, context: CallbackContext):
     query_received = update.message.text[7:]
     if query_received == "jackpot" or query_received == " jackpot":
         query_received = "7 pm CST"
-
-    if query_received.lower() == "christmas":
+    elif query_received.lower() == "christmas":
+        logging.info("requesting timeto christmas")
         query_received = "25 december"
 
     higher, time_to = time_util.get_time_diff(query_received)
