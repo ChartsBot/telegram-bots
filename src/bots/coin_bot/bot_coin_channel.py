c377e257766cf4ede1b9435798baa138b29e5797
ben <rotted_ben@protonmail.com>
2020-12-17 13:38:05 +0100

    ann channel better

--- src/bots/coin_bot/bot_coin_channel.py
+++ src/bots/coin_bot/bot_coin_channel.py
@@ -85,6 +85,8 @@ check_gas_interval_second = 900
 check_tweets_interval_second = 900
 check_biz_interval_second = 1000
 
+already_checked_tx = []
+list_channels = {}  # simple kv storage that stores the date of the last action made by the channel
 
 @dataclass(frozen=True)
 class Channel:
@@ -92,6 +94,13 @@ class Channel:
     ticker: str
     contract: str
     pair_contract: str
+
+
+def _should_send(channel: Channel, diff_time: int=300) -> bool:
+    if channel in list_channels:
+        now = int(time.time())
+        return now - list_channels[channel] < diff_time
+    return False
 
 
 @cached(cache=TTLCache(maxsize=1024, ttl=60))
@@ -130,48 +139,51 @@ def get_candlestick(context: CallbackContext):
     trending = util.get_banner_txt(zerorpc_client_data_aggregator)
 
     for channel in get_my_channels():
+        if _should_send(channel):
+            (message, path, reply_markup_chart) = general_end_functions.send_candlestick_pyplot(channel.ticker, charts_path, k_days,
+                                                                                                k_hours, t_from,
+                                                                                                t_to, txt=trending, options=options)
 
-        (message, path, reply_markup_chart) = general_end_functions.send_candlestick_pyplot(channel.ticker, charts_path, k_days,
-                                                                                            k_hours, t_from,
-                                                                                            t_to, txt=trending, options=options)
-
-        context.bot.send_photo(chat_id=channel.channel_id, photo=open(path, 'rb'), caption=message, parse_mode="html")
+            context.bot.send_photo(chat_id=channel.channel_id, photo=open(path, 'rb'), caption=message, parse_mode="html")
 
 
 def get_price_token(context: CallbackContext):
     for channel in get_my_channels():
+        if _should_send(channel):
 
-        message = general_end_functions.get_price(channel.contract, channel.pair_contract, graphql_client_eth,
-                                                  graphql_client_uni, channel.ticker.upper(), decimals, uni_wrapper)
-        context.bot.send_message(chat_id=channel.channel_id, text=message, parse_mode='html', disable_web_page_preview=True)
+            message = general_end_functions.get_price(channel.contract, channel.pair_contract, graphql_client_eth,
+                                                      graphql_client_uni, channel.ticker.upper(), decimals, uni_wrapper)
+            context.bot.send_message(chat_id=channel.channel_id, text=message, parse_mode='html', disable_web_page_preview=True)
 
 
 # sends the current biz threads
 def get_biz(context: CallbackContext):
     for channel in get_my_channels():
-        base_url = "boards.4channel.org/biz/thread/"
+        if _should_send(channel):
+            base_url = "boards.4channel.org/biz/thread/"
 
-        word = '$' + channel.ticker
-        word_regex_friendly = word.replace('$', '\\$')
-        message = """Current /biz threads containing the word $WORD:
-    """.replace("WORD", channel.ticker)
-        threads_ids = scrap_websites_util.get_biz_threads(re.compile(word_regex_friendly))
-        for thread_id in threads_ids:
-            excerpt = thread_id[2] + " | " + thread_id[1]
-            message += base_url + str(thread_id[0]) + " -- " + excerpt[0: 100] + "[...] \n"
-        if not threads_ids:
-            meme_caption = "No current /biz/ thread containing the word $WORD. You can make one at boards.4chan.org/biz/.".replace(
-                "$WORD", word)
-            context.bot.send_message(chat_id=channel.channel_id, text=meme_caption, disable_web_page_preview=True)
-        else:
-            context.bot.send_message(chat_id=channel.channel_id, text=message, disable_web_page_preview=True)
+            word = '$' + channel.ticker
+            word_regex_friendly = word.replace('$', '\\$')
+            message = """Current /biz threads containing the word $WORD:
+        """.replace("WORD", channel.ticker)
+            threads_ids = scrap_websites_util.get_biz_threads(re.compile(word_regex_friendly))
+            for thread_id in threads_ids:
+                excerpt = thread_id[2] + " | " + thread_id[1]
+                message += base_url + str(thread_id[0]) + " -- " + excerpt[0: 100] + "[...] \n"
+            if not threads_ids:
+                meme_caption = "No current /biz/ thread containing the word $WORD. You can make one at boards.4chan.org/biz/.".replace(
+                    "$WORD", word)
+                context.bot.send_message(chat_id=channel.channel_id, text=meme_caption, disable_web_page_preview=True)
+            else:
+                context.bot.send_message(chat_id=channel.channel_id, text=message, disable_web_page_preview=True)
 
 
 def get_twitter(context: CallbackContext):
     for channel in get_my_channels():
         tweets_of_the_last_minutes = int(check_tweets_interval_second / 60) * 2
         res = scrap_websites_util.get_last_tweets(twitter, channel.ticker, tweets_of_the_last_minutes)
-        context.bot.send_message(chat_id=channel.channel_id, text=res, parse_mode='html', disable_web_page_preview=True)
+        if "unable to find" not in res:
+            context.bot.send_message(chat_id=channel.channel_id, text=res, parse_mode='html', disable_web_page_preview=True)
 
 
 def get_gas_average(context: CallbackContext):
@@ -192,9 +204,6 @@ def get_trending(context: CallbackContext):
         context.bot.send_message(chat_id=channel.channel_id, text=res)
 
 
-already_checked_tx = [] 
-
-
 def get_actions(context: CallbackContext):
     print("checking monitors")
     global already_checked_tx
@@ -212,6 +221,10 @@ def get_actions(context: CallbackContext):
                                                                                     graphql_client_uni, options, amount=100, blacklist=already_checked_tx)
             already_checked_tx += ids
             if latest_actions_pretty is not None:
+
+                now = int(time.time())
+                list_channels[channel] = now
+
                 links = '<a href="etherscan.io/token/' + channel.contract + '">Etherscan</a> | <a href="https://app.uniswap.org/#/swap?inputCurrency=' + channel.contract + '">Buy on uniswap</a>'
 
                 message = "🚀Actions of the last minute: \n\n" + latest_actions_pretty + '\n\n' + links
