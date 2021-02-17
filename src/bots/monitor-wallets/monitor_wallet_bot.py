310e01442de2b2bf08c2c7bfd05a09f3b85ee3b6
ben <rotted_ben@protonmail.com>
2021-02-17 19:46:33 +0100

    added monitor wallet

--- src/bots/monitor-wallets/monitor_wallet_bot.py
+++ src/bots/monitor-wallets/monitor_wallet_bot.py
@@ -161,7 +161,7 @@ def view_wallets(update: Update, context: CallbackContext):
         else:
             message = "You are currently monitoring the following wallets:\n"
             for wallet in list_wallets:
-                message += wallet.keys()[0] + ' - ' + wallet['name'] + '\n'
+                message += list(wallet.keys())[0] + ' - ' + wallet['name'] + '\n'
             context.bot.send_message(chat_id=chat_id, text=message)
 
 
